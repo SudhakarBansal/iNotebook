3a2bee9ff6e767803eaa6c9eab261d64eae3c55b
Sudhakar <sidhu1171bansal@gmail.com>
2023-10-18 14:59:57 +0530

    Fetching notes from db

--- iNoteBook.php
+++ iNoteBook.php
@@ -1,3 +1,16 @@
+<!-- INSERT INTO `Notes` (`Sno`, `Title`, `Description`, `Time`) VALUES (NULL, 'Buy books', 'Go to market and buy some books for reading', current_timestamp()); -->
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "iNoteBook";
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Sorry ! Connection cannot be established due to : " . mysqli_connect_error());
+}
+
+?>
 <!doctype html>
 <html lang="en">
 
@@ -25,13 +38,36 @@
             </div>
             <div class="mb-5">
                 <label for="desc" class="form-label">Description : </label>
-                <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+                <textarea class="form-control" id="desc" name="desc" rows="2"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
-    <div class="container">
-        <?php ?>
+    <div class="container mt-4">
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM `Notes`";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>
+                    <th scope='row'>" . $row['Sno'] . "</th>
+                    <td>" . $row['Title'] . "</td>
+                    <td>" . $row['Description'] . "</td>
+                    <td>Actions</td>
+                </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
